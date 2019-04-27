fe0013a4374f88477e5052938372070c36a776e5
Dan Goje <gojedan98@gmail.com>
2019-04-27 04:35:30 +0100

    Handle base controller being pased to router

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -1,10 +1,13 @@
 import { IRoute } from '../interfaces/Route';
 
-abstract class Controller {
+class Controller {
   // @ts-ignore
   protected $routes: IRoute[];
 
   public getRoutes(): IRoute[] {
+    if (this.constructor.name === 'Controller') {
+      throw new Error('Passing base controller class to router is strictly prohibited.');
+    }
     if (!this.$routes) {
       throw new Error('Invalid controller. Did you use routing decorators?');
     }
