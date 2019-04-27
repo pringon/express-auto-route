2791ec732598a6130842a2cb1a353857867d7d3a
Dan Goje <gojedan98@gmail.com>
2019-04-27 04:39:07 +0100

    Reverse Controller to be an abstract class

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -1,13 +1,10 @@
 import { IRoute } from '../interfaces/Route';
 
-class Controller {
+abstract class Controller {
   // @ts-ignore
   protected $routes: IRoute[];
 
   public getRoutes(): IRoute[] {
-    if (this.constructor.name === 'Controller') {
-      throw new Error('Passing base controller class to router is strictly prohibited.');
-    }
     if (!this.$routes) {
       throw new Error('Invalid controller. Did you use routing decorators?');
     }
