0027c56639f1fe971c1c7642a69eac6d104e8a7f
Dan Goje <gojedan98@gmail.com>
2019-04-27 04:24:05 +0100

    Remove hello world file

--- src/controllers/Example.controller.ts
+++ src/controllers/Example.controller.ts
@@ -7,14 +7,12 @@ import Controller from './Controller';
 // @ts-ignore
 import { controller, get } from '../decorators/routing';
 
-import { hello } from '../helloWorld';
-
 @controller('/example')
 class ExampleController extends Controller {
 
   @get('/')
   hello(req: Request, res: Response, next: NextFunction) {
-    res.json({ message: hello() });
+    res.json({ message: 'Hello world!' });
   }
 }
 
