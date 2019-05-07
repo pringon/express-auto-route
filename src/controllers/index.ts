9bec755fc5da3b150289c2796ec3d5c4ab75300e
Dan Goje <gojedan98@gmail.com>
2019-05-07 09:08:19 +0100

    Improve controllers index file

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -12,8 +12,6 @@ const basename = path.basename(__filename);
 const getPath = (file: string): string => path.join(__dirname, file);
 
 /**
- * Normal fs.readdir takes a callback as its 3rd parameter so I am using promisify
- * to make it return a promise which allows for cleaner code using async/await.
  * @param {string} path path to directory.
  * @param {object} options encoding options.
  * @returns {Promise<string[]>} promise that will resolve to names of files in directory.
@@ -30,17 +28,16 @@ const importModules = async (controllerFiles: string[]): Promise<any> => (
 );
 
 /**
- * Take the name of a file checks that the file is actually a controller.
+ * Takes the name of a file and checks that the file is actually a controller.
  * @param {string} file name of file being checked
  * @returns {boolean} result of checking whether the file is a controller or not.
  */
 const filterControllers = (file: string): boolean => (
-  (file.indexOf('.') !== 0) && (file !== basename)
-    && (file.slice(-14, -2) === '.controller.')
+  (file[0] !== '.') && (file.indexOf('.controller.') !== -1)
 );
 
 /**
- * Asynchronously returns all controllers in src/controllers.
+ * Asynchronously returns all controllers in this directory.
  * @returns {Promise<ControllerClass[]>} promise that resolves to array of controllers.
  */
 export const getControllers = (): Promise<ControllerClass[]> => {
